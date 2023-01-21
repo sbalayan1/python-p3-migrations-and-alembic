af18e9eed46157d5f0f4c6969b3e00c0af87fe1c
Sean <balayans2014@yahoo.com>
2023-01-21 13:41:22 -0800

    complete

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,23 @@
 ##### #!/usr/bin/env python3: we will not need this anymore because we won't be executing our models.py as a script
 
-
-from sqlalchemy import create_engine
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///migrations_test.db')
 
 Base = declarative_base()
+
+class Students(Base):
+    __tablename__= 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrollment_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
