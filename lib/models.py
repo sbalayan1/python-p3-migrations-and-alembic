a4d98214fa379364446643f5288f93d842592ade
Sean <balayans2014@yahoo.com>
2023-01-21 09:22:55 -0800

    update

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+##### #!/usr/bin/env python3: we will not need this anymore because we won't be executing our models.py as a script
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///migrations_test.db')
